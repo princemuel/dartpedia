5438e1ffa036ecf0d625da4081de2e375146fb07
Prince Muel <vansomecsam@gmail.com>
2026-08-29 19:36:11 +0100

    feat: extend app with enums and graceful error handling

---
+++ command_runner/lib/src/exceptions.dart
@@ -0,0 +1,21 @@
+class ArgumentException(
+  super.message, [
+  this.command,
+  this.argumentName,
+  super.source,
+  super.offset,
+]) extends FormatException {
+  /// The command that was parsed before discovering the error.
+  ///
+  /// This will be empty if the error was on the root parser.
+  final String? command;
+
+  /// The name of the argument that was being parsed when the error was
+  /// discovered.
+  final String? argumentName;
+
+  @override
+  String toString() {
+    return 'ArgumentException: $message';
+  }
+}
