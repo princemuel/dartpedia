dc76d777fbfc5b719072be1fb112e22795824fbc
Prince Muel <vansomecsam@gmail.com>
2026-08-29 19:35:45 +0100

    feat: extend app with enums and graceful error handling

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -2,9 +2,21 @@ import 'package:command_runner/command_runner.dart';
 
 const version = '0.0.1';
 
-void main(List<String> arguments) async {
-  var runner = CommandRunner();
-  await runner.run(arguments);
+void main(List<String> arguments) {
+  var commandRunner = CommandRunner(
+    onOutput: (String output) async {
+      await write(output);
+    },
+    onError: (Object error) {
+      if (error is Error) {
+        throw error;
+      }
+      if (error is Exception) {
+        print(error);
+      }
+    },
+  )..addCommand(HelpCommand());
+  commandRunner.run(arguments);
 }
 
 // void main(List<String> arguments) {
